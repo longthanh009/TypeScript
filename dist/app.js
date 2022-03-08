e173dfb94f3c06a2f8e82706d31a45f14ce0c824
khuatlong0902 <khuatlong0902@gmail.com>
2022-03-08 18:04:17 +0700

    update file

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const a = 10;
+const b = 20;
+const myName = "Long";
+const sinhVien = {};
+const listSv = [{ id: 1, name: "Long" }, { id: 2, name: "Thanh" }];
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+export {};
+
+const a:number =10;
+const b:number =20;
+const myName:string = "Long";
+const sinhVien :object = {};
+type SinhVien = {
+    id:number,
+    name : string,
+}
+const listSv :SinhVien[] =[{id:1,name:"Long"},{id:2,name: "Thanh"}];
