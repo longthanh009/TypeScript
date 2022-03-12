4a832010e5efa68df75d79ec63e50b0dd4a6b525
khuatlong0902 <khuatlong0902@gmail.com>
2022-03-12 17:14:32 +0700

    update b2

--- src/app.ts
+++ src/app.ts
@@ -8,4 +8,12 @@ type SinhVien = {
     id:number,
     name : string,
 }
+type giangVien = {
+    khoa : string,
+}
+type Person = SinhVien | giangVien;
+const addSinhVien = <T extends SinhVien> (sinhVien : T[]) =>{
+    const data = sinhVien.map(item => `<div> <h1>sinhVien.name</h1> </div>`)
+}
 const listSv :SinhVien[] =[{id:1,name:"Long"},{id:2,name: "Thanh"}];
+addSinhVien([{id: 1, name: "Long"},{id: 2 ,name: "Thanh"}]);
